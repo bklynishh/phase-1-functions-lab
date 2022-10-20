e5859437ab3df2959f8346a4e8144591e4473eb1
Hannah Maratea <maratea101@gmail.com>
2022-10-19 20:12:34 -0400

    commit tree e2fc2168b105496707064b6b03de2de98190f80d

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+// Function 1 - distanceFromHqInBlocks
+function distanceFromHqInBlocks (blocks) {
+    if (blocks > 42) {
+        return blocks - 42
+    } else {
+       return 42 - blocks
+    }
+}
+
+// Function 2 - distanceFromHqInFeet
+function distanceFromHqInFeet (blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264
+}
+
+// Function 3 - distanceTravelledInFeet
+function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264
+    } else {
+        return (start - destination) * 264
+    }
+}
+
+// Function 4 - calculatesFarePrice
+function calculatesFarePrice (start, destination) {
+    let distance = distanceTravelledInFeet (start, destination)
+    if (distance <= 400) {
+        return 0
+    }
+    if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02
+    }
+    if (distance > 2000, distance < 2500) {
+        return 25
+    }
+    if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
